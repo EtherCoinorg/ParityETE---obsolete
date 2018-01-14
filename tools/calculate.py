48ef6ab705a49756974c41f7a5c3bfd4bb69d202
ziminq <ziminq@gmail.com>
2018-01-14 18:42:31 +0800

    etg: modify the parameters

--- tools/calculate.py
+++ tools/calculate.py
@@ -1,10 +1,9 @@
 
 
-total_supply_at_hard_fork = 0 # to be supplied
+total_supply_at_hard_fork = 98045587 # to be supplied
 
-hardfork_number = 4730666
 halving_interval = 3000000   # 2628000 ~ 1 year, assume 10 ~ 15 seconds for each block
-block_reward = 8 * 1000000000000000000   # in unit of wei
+block_reward = 7 * 1000000000000000000   # in unit of wei
 
 
 total_supply = 0
@@ -12,7 +11,7 @@ total_supply = 0
 for interval in range(1, 65):
 
     each_block_reward = block_reward >> interval
-    rewards_in_this_interval = each_block_reward * halving_interval
+    rewards_in_this_interval = each_block_reward * halving_interval * 1.03 # including the uncle rewards
     total_supply = total_supply + rewards_in_this_interval
 
     print("interval {}: each block reward: {} total rewards in this reward: {} sum till now: {}".format(interval, each_block_reward, rewards_in_this_interval, total_supply))
@@ -21,4 +20,5 @@ for interval in range(1, 65):
 total_supply = (total_supply + total_supply_at_hard_fork) / 1000000000000000000
 
 print("total supply: {} eth".format(total_supply))
+print(" ~ {} w eth".format(total_supply / 10000.0))
 
